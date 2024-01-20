f4cc03d0a219436fe84cf32daa78bf510702ad0c
Lann Martin <lann.martin@fermyon.com>
2024-01-20 13:05:26 -0500

    Make lex::Keyword pub

--- src/lex.rs
+++ src/lex.rs
@@ -4,56 +4,56 @@ use std::fmt::Display;
 
 use logos::{Lexer, Logos, Span};
 
-/// A [`logos::Logos`] implementation that represents a WAVE token.
+/// A WAVE token implementing [`logos::Logos`]
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Logos)]
 #[logos(error = Option<Span>)]
 #[logos(skip r"[ \t\n\r]+")]
 #[logos(subpattern label_word = r"[a-z][a-z0-9]*|[A-Z][A-Z0-9]*")]
 #[logos(subpattern char_escape = r#"\\['"tnr\\]|\\u\{[0-9a-fA-F]{1,6}\}"#)]
 pub enum Token {
-    /// The `{` symbol.
+    /// The `{` symbol
     #[token("{")]
     BraceOpen,
-    /// The `}` symbol.
+    /// The `}` symbol
     #[token("}")]
     BraceClose,
 
-    /// The `(` symbol.
+    /// The `(` symbol
     #[token("(")]
     ParenOpen,
-    /// The `)` symbol.
+    /// The `)` symbol
     #[token(")")]
     ParenClose,
 
-    /// The `[` symbol.
+    /// The `[` symbol
     #[token("[")]
     BracketOpen,
-    /// The `]` symbol.
+    /// The `]` symbol
     #[token("]")]
     BracketClose,
 
-    /// The `:` symbol.
+    /// The `:` symbol
     #[token(":")]
     Colon,
 
-    /// The `,` symbol.
+    /// The `,` symbol
     #[token(",")]
     Comma,
 
-    /// A number literal.
+    /// A number literal
     #[regex(r"-?(0|([1-9][0-9]*))(\.[0-9]+)?([eE][-+]?[0-9]+)?")]
     #[token("-inf")]
     Number,
 
-    /// A label.
+    /// A label or keyword
     #[regex(r"%?(?&label_word)(-(?&label_word))*")]
     LabelOrKeyword,
 
-    /// A char literal.
+    /// A char literal
     #[regex(r#"'([^\\']{1,4}|(?&char_escape))'"#, validate_char)]
     Char,
 
-    /// A string literal.
+    /// A string literal
     #[regex(r#""([^\\"]|(?&char_escape))*""#)]
     String,
 }
@@ -73,7 +73,10 @@ fn validate_char(lex: &mut Lexer<Token>) -> Result<(), Option<Span>> {
     }
 }
 
-pub(crate) enum Keyword {
+/// A WAVE keyword
+#[derive(Clone, Copy, Debug, PartialEq)]
+#[allow(missing_docs)]
+pub enum Keyword {
     True,
     False,
     Some,
@@ -85,6 +88,7 @@ pub(crate) enum Keyword {
 }
 
 impl Keyword {
+    /// Returns any keyword exactly matching the given string.
     pub fn decode(raw_label: &str) -> Option<Self> {
         Some(match raw_label {
             "true" => Self::True,
@@ -96,6 +100,21 @@ impl Keyword {
             "inf" => Self::Inf,
             "nan" => Self::Nan,
             _ => return None,
+        })
+    }
+}
+
+impl Display for Keyword {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            Keyword::True => "true",
+            Keyword::False => "false",
+            Keyword::Some => "some",
+            Keyword::None => "none",
+            Keyword::Ok => "ok",
+            Keyword::Err => "err",
+            Keyword::Inf => "inf",
+            Keyword::Nan => "nan",
         })
     }
 }
